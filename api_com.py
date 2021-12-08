54fd33b0ed4714406e1a8a1355ecc8ff2839223d
jlrzarcor <jlrzarate@hotmail.com>
2021-12-07 22:23:19 -0600

    working api @ alive prediction, #5, 2 togo

--- api_com.py
+++ api_com.py
@@ -1,91 +1,56 @@
-import datetime
+from flask import Flask, request
+import psycopg2
+import psycopg2.extras
+import json
 import os
-from flask import Flask
-from flask_restplus import Api, Resource, fields
-from flask_sqlalchemy import SQLAlchemy
-from src.utils import creds as cred
-
-# import json
-
-# from src.utils.general import get_db_conn_sql_alchemy
-
-# mysql://username:password@server/db
-# "postgresql://postgres:Equ1p0_2@rds-dpa.cheenr0nt3eh.us-east-2.rds.amazonaws.com:5432/food"
-
-# db_conn_str = get_db_conn_sql_alchemy('../../conf/local/credentials.yaml')
-
-db_conn_str = f'postgresql://{os.environ["PGUSR"]}:{os.environ["PGPASS"]}@db:5432/postgres'
-
+#import flask
+import pickle as pkl
+import sklearn
+import numpy as np
+import pandas as pd
+database_uri = "postgresql://postgres:postgres@db:5432/postgres"
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = db_conn_str
-api = Api(app)
-db = SQLAlchemy(app)
-
-
-class DataCCD(db.Model):
-    __table_args__ = {'schema': 'clean'}
-    __tablename__ = 'variables'
-
-    mopllaag = db.Column(db.Integer, primary_key=True)
-    mink123m = db.Column(db.Integer)
-    ppersaut = db.Column(db.Integer)
-    pwaoreg = db.Column(db.Integer)
-    pbrand = db.Column(db.Integer)
-    aplezier = db.Column(db.Integer)
-    afiets = db.Column(db.Integer)
-    caravan = db.Column(db.Integer)
-    date_ing = db.Column(db.DateTime)
-
-    def __repr__(self):
-        return u'<{self.__class__.__name__}: {self.id}>'.format(self=self)
-
-
-all_data = api.model("data_CCP", {
-    'mopllaag': fields.Integer,
-    'mink123m': fields.Integer,
-    'ppersaut': fields.Integer,
-    'pwaoreg': fields.Integer,
-    'pbrand': fields.Integer,
-    'aplezier': fields.Integer,
-    'afiets': fields.Integer,
-    'caravan': fields.Integer,
-    'date_ing': fields.Date
-})
-
-all_data_list = api.model('all_data_clean', {
-    'all_data': fields.Nested(all_data)
-})
-
-
-# endpoints
-@api.route('/')
-class ApiInfo(Resource):
-    def get(self):
-        return {'Api_CCD': 'Api de Computacion para Ciencia de Datos'}
-
-
-@api.route('/all_data/')
-class AllData(Resource):
-    @api.marshal_with(all_data_list, as_list=True)
-    def get(self):
-        all_d = DataCCD.query.all()
-        a_d = []
-        for element in all_d:
-            a_d.append({
-                'mopllaag': element.mopllaag,
-                'mink123m': element.mink123m,
-                'ppersaut': element.ppersaut,
-                'pwaoreg': element.pwaoreg,
-                'pbrand': element.pbrand,
-                'aplezier': element.aplezier,
-                'afiets': element.afiets,
-                'caravan': element.caravan,
-                'date_ing': element.date_ing
-            })
-
-        return {'all_data': a_d}
-
-
-if __name__ == '__main__':
-    # app.run(debug=True)
+conn = psycopg2.connect(database_uri)
+@app.route("/")
+def home():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+    cur.execute("select * from variables limit 10;")
+    results = cur.fetchall()
+    cur.close()
+    return json.dumps([x._asdict() for x in results], default=str)
+@app.route("/user", methods=["POST", "GET"])
+def user():
+    if request.method == "GET":
+        cur = conn.cursor(cursor_factory=psycopg2.extras.NamedTupleCursor)
+        user_id = request.args.get("id")
+        cur.execute(f"select * from users where id={user_id}")
+        results = cur.fetchone()
+        cur.close()
+        return json.dumps(results._asdict(), default=str)
+    if request.method == "POST":
+        user = request.json
+        cur = conn.cursor()
+        cur.execute(
+            "insert into users (name, lastname, age) values (%s, %s, %s)",
+            (user["name"], user["lastname"], user["age"]),
+        )
+        conn.commit()
+        cur.execute("SELECT LASTVAL()")
+        user_id = cur.fetchone()[0]
+        cur.close()
+        return json.dumps({"user_id": user_id})
+@app.route("/users", methods=["POST", "GET"])
+def users():
+    if request.method == "GET":
+        return "dummy"
+    if request.method == "POST":
+        inputs = request.json
+        predecir = inputs["predecir"]
+        users_list = [[predecir["mopllaag"], predecir["mink123m"],predecir["ppersaut"],
+        predecir["pwaoreg"], predecir["pbrand"],predecir["aplezier"], predecir["afiets"]]]
+        with open("/jlrzarcor-ITAM-ecomp2021-Ramis-finalprjct/src/temp/trained_models/modelo_lr_pkl", "rb") as f:
+            model_loaded = pkl.load(f)
+        pred = model_loaded.predict(users_list)
+    return json.dumps({"valor_de_prediccion": pred[0].astype(str)})
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=8080)
